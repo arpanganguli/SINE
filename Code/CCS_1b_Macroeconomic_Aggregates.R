0f89abbe3ea554ae38b53c9bca8da28f394c3e58
Arpan Ganguli <arpan@alumni.lse.ac.uk>
2021-03-09 19:34:42 +0000

    Changed R code

--- Code/CCS_1b_Macroeconomic_Aggregates.R
+++ Code/CCS_1b_Macroeconomic_Aggregates.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(plotly)
+library(scales)
 
 df <- read.csv("~/Documents/Professional/Projects/SINE/Database/Time_Series/Macroeconomic_Aggregates.csv", stringsAsFactors = TRUE)
 View(df)
@@ -9,5 +10,7 @@ df$Year <- factor(df$Year, levels = df$Year[order(df$Gross.Domestic.Product)])
 
 p <- ggplot(data = df, aes(x=Year, y=Gross.Domestic.Product)) +
             geom_bar(stat = "identity", fill = "darkblue") +
-            theme_minimal()
+            theme_minimal() +
+            theme(axis.text.x = element_text(face = "bold", color = "#993333", 
+                                     size = 12, angle = 45))
 ggplotly(p)
